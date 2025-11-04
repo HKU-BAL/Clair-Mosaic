31c34eb491be6fa37527b87ea685b785385be4dc
JasonCLEI <765603496@qq.com>
2025-11-04 15:56:57 +0800

    updated pacbio_hifi_quick_demo.sh

--- demo/pacbio_hifi_quick_demo.sh
+++ demo/pacbio_hifi_quick_demo.sh
@@ -32,6 +32,7 @@ docker run -it \
   --platform hifi_revio \
   --output_dir ${OUTPUT_DIR} \
   --region chr20:47000000-47100000 \
+  --enable_baymgd_tagging \
   --enable_mosaicbase_tagging \
   --enable_post_filtering \
   --enable_indel_calling
@@ -55,6 +56,7 @@ docker run -it \
   --platform hifi_revio \
   --output_dir ${OUTPUT_DIR} \
   --region chr20:47000000-47100000 \
+  --enable_baymgd_tagging \
   --enable_mosaicbase_tagging \
   --enable_post_filtering \
   --enable_indel_calling
